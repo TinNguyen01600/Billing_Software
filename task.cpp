67678ab3e2ee85b82743226a19cd634edaa1f337
TinNguyen <ductin16111@gmail.com>
2022-05-05 00:17:59 +0300

    update function print_home_screen()

--- task.cpp
+++ task.cpp
@@ -1,6 +1,14 @@
 #include "mylib.hh"
 
 int print_home_screen(){
-    cout << "*********** Restaurant ***********" << endl << endl;
-    cout << "  Select an operation:" << endl << endl;
+    system("cls");
+    cout << endl << "============ Restaurant ============" << endl << endl;
+    
+    cout << "1. Create new invoice" << endl;
+    cout << "2. Show all invoices" << endl;
+    cout << "3. Search" << endl;
+    cout << "4. Exit" << endl << endl;
+    cout << "Select an operation: ";
+    int choice; cin >> choice;
+    return choice;
 }
