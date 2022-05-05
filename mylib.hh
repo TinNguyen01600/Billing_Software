97c22f0f33c0057b68e01d689ff6dcbd42aab2ab
TinNguyen <ductin16111@gmail.com>
2022-05-05 13:45:26 +0300

    add class Customer{} and Item{}

--- mylib.hh
+++ mylib.hh
@@ -1,7 +1,24 @@
 #include <iostream>
 #include <cstring>
+#include <string>
 #include <conio.h>
 using namespace std;
+
+const int LEN = 50;
+
+class Customer{
+    char name[LEN];
+    int item_indices;
+    friend class Item;
+};
+
+//------------------------------------------------------------------------
+
+class Item{
+
+};
+
+//------------------------------------------------------------------------
 
 int print_home_screen();
 void loop();
