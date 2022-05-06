55932b75c5698dac314187a782be7bd9d6ac0dbb
TinNguyen <ductin16111@gmail.com>
2022-05-06 13:17:05 +0300

    update class Customer, add struct item inside class Customer

--- mylib.hh
+++ mylib.hh
@@ -11,20 +11,27 @@ const int LEN = 50;
 class Customer{
     char name[LEN];
     int item_indices;
-    friend class Item;
 public:
-    void set_data(char *, int);
+    struct Item{
+        string food;
+        int unit_price, quantity;
+    };
+    void set_data(char *name, int item_indices){
+        strcpy(this->name, name);
+        this->item_indices = item_indices;
+        Item *item = new Item[item_indices];
+        for(int i = 0; i<item_indices; i++){
+            cout << endl << "Enter item " << i+1 << ":\t\t";
+            getline(cin, item[i].food);
+            cout << "Enter the unit price:\t";
+            cin >> item[i].unit_price;
+            cout << "Enter the quantity:\t";
+            cin >> item[i].quantity;
+        }
+    };
     Customer *search(char *name_search){
         return(strcmp(name_search,this->name) == 0)?this : NULL;
     };
-};
-
-//------------------------------------------------------------------------
-
-class Item{
-    char food[LEN];
-    int unit_price, quality;
-
 };
 
 //------------------------------------------------------------------------
